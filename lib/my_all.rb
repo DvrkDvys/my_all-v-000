30e3b7444a9a2065c4dd11ca39c26a443e0cb26c
Jordan Harris <jharris@wework.com>
2018-06-27 20:44:06 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,32 +1,38 @@
 require 'pry'
 
 def my_all?(collection)
-
+ i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
 end
 
 
 
-  it "can handle an empty collection" do
-    counter=0
-    my_all?([]) do |x|
-      counter+=1
-    end
-    expect(counter).to eq(0)
-  end
+#   it "can handle an empty collection" do
+#     counter=0
+#     my_all?([]) do |x|
+#       counter+=1
+#     end
+#     expect(counter).to eq(0)
+#   end
 
-  it "yields the correct element to the block" do
-    yielded = []
-    my_all?(collection) do |number|
-      yielded.push number
-    end
-    expect(yielded).to eq([1,2,3])
-  end
+#   it "yields the correct element to the block" do
+#     yielded = []
+#     my_all?(collection) do |number|
+#       yielded.push number
+#     end
+#     expect(yielded).to eq([1,2,3])
+#   end
 
-  it "returns false when the block condition is not met" do
-    expect(my_all?(collection) {|i| i < 3}).to eq(false)
-  end
+#   it "returns false when the block condition is not met" do
+#     expect(my_all?(collection) {|i| i < 3}).to eq(false)
+#   end
   
-  it "returns true when the block condition is met" do
-    expect(my_all?(collection) {|i| i < 4}).to eq(true)
-  end
-end
+#   it "returns true when the block condition is met" do
+#     expect(my_all?(collection) {|i| i < 4}).to eq(true)
+#   end
+# end
